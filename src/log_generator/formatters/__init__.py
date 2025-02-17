542fcb408da373b0e92282258b836d7953e1f702
Brett Larson <larson.brett.s@gmail.com>
2025-02-16 19:57:35 -0500

    Update __init__.py

--- src/log_generator/formatters/__init__.py
+++ src/log_generator/formatters/__init__.py
@@ -1,4 +1,6 @@
 from .base_formatter import BaseFormatter
 from .json_formatter import JSONFormatter
+from .text_formatter import TextFormatter
+from .multi_line import MultilineFormatter
 
-__all__ = ['BaseFormatter', 'JSONFormatter']
+__all__ = ['BaseFormatter', 'JSONFormatter', 'TextFormatter', 'MultilineFormatter']
